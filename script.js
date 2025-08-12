2ea443424f327b13d87f7b91fd1bcf00a0f8c936
Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>
2025-08-12 07:33:38 +0000

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu-links");
-const icon = document.querySelector(".hamburger-icon");
-
-function toggleMenu() {
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
